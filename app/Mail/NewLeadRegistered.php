454e2b6cf706b9729b8cf4ac8a1064b0c09a20bc
helissonms <helisson.ms@gmail.com>
2017-09-17 17:15:08 -0300

    Envio do email por fila.

--- app/Mail/NewLeadRegistered.php
+++ app/Mail/NewLeadRegistered.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class NewLeadRegistered extends Mailable
+class NewLeadRegistered extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
