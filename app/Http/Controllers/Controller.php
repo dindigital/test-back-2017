4f269da356583c93156f1ee5d3564c6a3937dff4
helissonms <helisson.ms@gmail.com>
2017-09-17 15:18:43 -0300

    API Rest para os médicos.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param int $statusCode
+     * @return \Illuminate\Http\Response
+     */
+    protected function abort($statusCode)
+    {
+        return abort(
+            $statusCode,
+            Response::$statusTexts[$statusCode]
+        );
+    }
 }
--- app/Http/Controllers/DoctorsController.php
+++ app/Http/Controllers/DoctorsController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Doctor;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class DoctorsController extends Controller
 {
+
+    public function index()
+    {
+        $doctors = $this->model()->paginate();
+
+        return response()->json($doctors, Response::HTTP_OK);
+    }
+
 	/**
      * Display the specified resource.
      *
@@ -15,17 +23,27 @@ class DoctorsController extends Controller
      */
     public function show($id)
     {
-    	$doctor = $this->model()->findOrFail($id);
+        $doctor = $this->model()->find($id);
+
+        if (is_null($doctor)) {
+            $this->abort(Response::HTTP_NOT_FOUND);
+        }
+
+        if (request()->expectsJson()) {
+            return response()->json([
+                'data' => $doctor
+            ], Response::HTTP_OK);
+        }
 
     	return view('doctors.show')->with('doctor', $doctor);
     }
-
+    
 	/**
      * Get the instance of the Doctor model.
      *
      * @return App\Model\Doctor
      */
-    public function model()
+    private function model()
     {
     	return new Doctor;
     }
